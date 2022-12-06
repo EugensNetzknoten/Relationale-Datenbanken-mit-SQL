18f904c836a552a9750b02b702d221727e8eb83f
EugensNetzknoten <119836515+EugensNetzknoten@users.noreply.github.com>
2022-12-06 03:17:56 +0100

    Add files via upload

--- 001-DB-Create-Scripts/001-01-EG-CREATE-DB-MiniBank.sql
+++ 001-DB-Create-Scripts/001-01-EG-CREATE-DB-MiniBank.sql
@@ -1,1 +1,23 @@
+USE [master];
+GO
 
+
+
+--DROP DATABASE [MiniBank]
+--GO
+
+CREATE DATABASE [MiniBank]
+	CONTAINMENT = NONE
+ON  PRIMARY 
+	( NAME = N'MiniBank', FILENAME = N'C:\SQL-Kurs\DB\Projekt\MiniBank.mdf' , SIZE = 8192KB , FILEGROWTH = 10%)
+LOG ON 
+	( NAME = N'MiniBank_log', FILENAME = N'C:\SQL-Kurs\DB\Projekt\MiniBank_log.ldf' , SIZE = 8192KB , FILEGROWTH = 10%)
+GO
+
+
+
+-- change owner to sa
+--USE [MiniBank]
+--GO
+--ALTER AUTHORIZATION ON DATABASE::[MiniBank] TO [sa]
+--GO
